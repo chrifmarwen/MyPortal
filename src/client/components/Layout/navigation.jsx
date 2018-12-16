2b871743e16f068bb20593f1211c5334a5650bff
Marwen Cherif <marouen.chrif@gmail.com>
2018-12-16 20:57:00 +0100

    Commenting out about me link for current version

--- src/client/components/Layout/navigation.jsx
+++ src/client/components/Layout/navigation.jsx
@@ -16,9 +16,9 @@ class Navigation extends Component {
             </button>
             <div className="collapse navbar-collapse" id="navbarResponsive">
               <ul className="navbar-nav ml-auto">
-                <li className="nav-item">
-                  <NavLink className="nav-link" to="/aboutme">About Me</NavLink>
-                </li>
+                {/*<li className="nav-item">*/}
+                {/*<NavLink className="nav-link" to="/aboutme">About Me</NavLink>*/}
+                {/*</li>*/}
                 {/*<li className="nav-item">*/}
                 {/*<a className="nav-link" href="#">Log In</a>*/}
                 {/*</li>*/}
