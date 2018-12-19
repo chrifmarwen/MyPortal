10b468d6c081db8b1b26c5c608ea35b10e17b910
Marwen Cherif <marouen.chrif@gmail.com>
2018-12-19 08:20:01 +0100

    Fix import

--- src/client/index.js
+++ src/client/index.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { HashRouter, Route, Switch } from 'react-router-dom'
-import { AboutMePage, HomePage, MyPortfolioPage } from './pages.jsx'
-
+import { AboutMePage, HomePage, MyPortfolioPage } from './pages'
 import Whoops404 from './components/Layout/Whoop404'
+import styles from './scss/application.scss'
 
 window.React = React
 render(
